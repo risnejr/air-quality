31748f2c6f2bcff8b4ae1fb674ec14fc19220fdd
Victor Risne <risne@pm.me>
2018-07-24 15:44:53 +0200

    Refactor

--- alarm_level/alarm_level.py
+++ alarm_level/alarm_level.py
@@ -13,67 +13,70 @@ from keras.optimizers import Adam
 from keras.utils import normalize
 
 class AlarmLevel:
-	def __init__(self, function_loc):
+	def __init__(self, function_loc, csv_file):
+		self.x = np.zeros((1, 4))
+		self.model = Sequential()
+		self.csv_file = csv_file
 		self.location = function_loc
+		self.node_ids = defaultdict(dict)
+		self.csv_file = csv_file
 		self.classes = ['Good', 'Ok', 'Bad']
 		self.answers = {'Good': np.array([[1, 0, 0]]),
 						'Ok':   np.array([[0, 1, 0]]),
 						'Bad':  np.array([[0, 0, 1]])}
 
-	def get_node_ids(self, csv_file):
-		node_ids = defaultdict(dict)
-
-		with open(csv_file, 'r') as f:
+	def read_node_ids(self):
+		with open(self.csv_file, 'r') as f:
 			header = next(f)
-			for row in csv.reader(f, delimiter=','):
-				node_ids[row[0]].update({row[1]: row[2]})
+			for row in csv.reader(f, skipinitialspace=True ,delimiter=','):
+				if len(row) == 3:
+					self.node_ids[row[0]].update({row[1]: row[2]})
 
-		return node_ids
+		try:
+			self.node_ids = self.node_ids[self.location]
+		except KeyError:
+			print("Asset couldn't be found in provided .csv file")
 
-	def run(self, data, x, model, node_ids):
-		if data.node_id == node_ids['vote']:
+	def run(self, data):
+		if data.node_id == self.node_ids['vote']:
 			y_true = self.answers[data.node_data.question_answers[0]]
-			x = normalize(x)
-			model = self.train(x, y_true, model)
-		elif data.node_id == node_ids['temp']:
-			x[0, 0] = data.node_data.data_point.coordinate.y
-		elif data.node_id == node_ids['pres']:
-			x[0, 1] = data.node_data.data_point.coordinate.y
-		elif data.node_id == node_ids['hum']:
-			x[0, 2] = data.node_data.data_point.coordinate.y
-		elif data.node_id == node_ids['gas']:
-			x[0, 3] = data.node_data.data_point.coordinate.y
-			if x.all():
-				x = normalize(x)
-				y_pred = self.predict(x, model)
+			self.x = normalize(self.x)
+			self.train(y_true)
+
+		elif data.node_id == self.node_ids['temp']:
+			self.x[0, 0] = data.node_data.data_point.coordinate.y
+
+		elif data.node_id == self.node_ids['pres']:
+			self.x[0, 1] = data.node_data.data_point.coordinate.y
+
+		elif data.node_id == self.node_ids['hum']:
+			self.x[0, 2] = data.node_data.data_point.coordinate.y
+
+		elif data.node_id == self.node_ids['gas']:
+			self.x[0, 3] = data.node_data.data_point.coordinate.y
+			if self.x.all():
+				self.x = normalize(self.x)
+				y_pred = self.predict()
 				print(self.classes[np.argmax(y_pred)], y_pred)
 
-		return x, model
-
 	def define_model(self):
-		model = Sequential()
-		model.add(Dense(32, activation='relu', input_shape=(4,)))
-		model.add(Dense(3, activation='softmax'))
-
+		self.model.add(Dense(32, activation='relu', input_shape=(4,)))
+		self.model.add(Dense(3, activation='softmax'))
 		adam = Adam()
-		model.compile(loss='categorical_crossentropy',
+		self.model.compile(loss='categorical_crossentropy',
 					optimizer=adam,
 					metrics=['accuracy'])
 
 		try:
-			model.load_weights('aq_model_weights.h5')
+			self.model.load_weights('aq_model_weights.h5')
 		except IOError:
 			print('No file named aq_model_weights.h5')
 
-		return model
+	def train(self, y):
+		self.model.fit(self.x, y, epochs=1)
 
-	def train(self, x, y, model):
-		model.fit(x, y, epochs=1)
-
-		return model
-
-	def predict(self, x, model):
-		return model.predict(x)
+	def predict(self):
+		return self.model.predict(self.x)
 
 	def grpc_stream(self):
 		channel = dial_grpc.dial()
@@ -83,17 +86,17 @@ class AlarmLevel:
 		return response
 
 	def main(self):
-		x = np.zeros((1, 4))
-		model = self.define_model()
-		response = self.grpc_stream()
-		node_ids = self.get_node_ids('node_id.csv')[self.location]
 		t0 = time.time()
+		self.define_model()
+		self.read_node_ids()
+		response = self.grpc_stream()
 
 		for data in response:
-			x, model = self.run(data, x, model, node_ids)
+			print(data)
+			self.run(data)
 			if time.time() - t0 > 60:
 				print('Saving model...')
-				model.save_weights('aq_model_weights.h5')
+				self.model.save_weights('aq_model_weights.h5')
 				t0 = time.time()
 
 		channel.close()
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from alarm_level.alarm_level import AlarmLevel
 
 if __name__ == '__main__':
-	desk = AlarmLevel('desk')
+	desk = AlarmLevel('desk', 'node_ids.csv')
 	desk.main()
