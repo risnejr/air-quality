3031d31271a7e0f0dfbf8c2d505494d4d2bd93e9
Victor Risne <risne@pm.me>
2018-07-25 09:17:43 +0200

    Adapt flake8 linting

--- alarm_level/alarm_level.py
+++ alarm_level/alarm_level.py
@@ -12,93 +12,93 @@ from keras.layers import Dense
 from keras.optimizers import Adam
 from keras.utils import normalize
 
+
 class AlarmLevel:
-	def __init__(self, function_loc, csv_file):
-		self.x = np.zeros((1, 4))
-		self.model = Sequential()
-		self.csv_file = csv_file
-		self.location = function_loc
-		self.node_ids = defaultdict(dict)
-		self.csv_file = csv_file
-		self.classes = ['Good', 'Ok', 'Bad']
-		self.answers = {'Good': np.array([[1, 0, 0]]),
-						'Ok':   np.array([[0, 1, 0]]),
-						'Bad':  np.array([[0, 0, 1]])}
 
-	def read_node_ids(self):
-		with open(self.csv_file, 'r') as f:
-			header = next(f)
-			for row in csv.reader(f, skipinitialspace=True ,delimiter=','):
-				if len(row) == 3:
-					self.node_ids[row[0]].update({row[1]: row[2]})
+    def __init__(self, function_loc, csv_file):
+        self.x = np.zeros((1, 4))
+        self.model = Sequential()
+        self.csv_file = csv_file
+        self.location = function_loc
+        self.node_ids = defaultdict(dict)
+        self.csv_file = csv_file
+        self.classes = ['Good', 'Ok', 'Bad']
+        self.answers = {'Good': np.array([[1, 0, 0]]),
+                        'Ok':   np.array([[0, 1, 0]]),
+                        'Bad':  np.array([[0, 0, 1]])}
 
-		try:
-			self.node_ids = dict(self.node_ids)
-			self.node_ids = self.node_ids[self.location]
-		except KeyError:
-			raise
+    def read_node_ids(self):
+        with open(self.csv_file, 'r') as f:
+            next(f)
+            for row in csv.reader(f, skipinitialspace=True, delimiter=','):
+                if len(row) == 3:
+                    self.node_ids[row[0]].update({row[1]: row[2]})
 
+        try:
+            self.node_ids = dict(self.node_ids)
+            self.node_ids = self.node_ids[self.location]
+        except KeyError:
+            raise
 
-	def run(self, data):
-		if data.node_id == self.node_ids['vote']:
-			y_true = self.answers[data.node_data.question_answers[0]]
-			self.x = normalize(self.x)
-			self.train(y_true)
+    def run(self, data):
+        if data.node_id == self.node_ids['vote']:
+            y_true = self.answers[data.node_data.question_answers[0]]
+            self.x = normalize(self.x)
+            self.train(y_true)
 
-		elif data.node_id == self.node_ids['temp']:
-			self.x[0, 0] = data.node_data.data_point.coordinate.y
+        elif data.node_id == self.node_ids['temp']:
+            self.x[0, 0] = data.node_data.data_point.coordinate.y
 
-		elif data.node_id == self.node_ids['pres']:
-			self.x[0, 1] = data.node_data.data_point.coordinate.y
+        elif data.node_id == self.node_ids['pres']:
+            self.x[0, 1] = data.node_data.data_point.coordinate.y
 
-		elif data.node_id == self.node_ids['hum']:
-			self.x[0, 2] = data.node_data.data_point.coordinate.y
+        elif data.node_id == self.node_ids['hum']:
+            self.x[0, 2] = data.node_data.data_point.coordinate.y
 
-		elif data.node_id == self.node_ids['gas']:
-			self.x[0, 3] = data.node_data.data_point.coordinate.y
-			if self.x.all():
-				self.x = normalize(self.x)
-				y_pred = self.predict()
-				print(self.classes[np.argmax(y_pred)], y_pred)
+        elif data.node_id == self.node_ids['gas']:
+            self.x[0, 3] = data.node_data.data_point.coordinate.y
+            if self.x.all():
+                self.x = normalize(self.x)
+                y_pred = self.predict()
+                print(self.classes[np.argmax(y_pred)], y_pred)
 
-	def define_model(self):
-		self.model.add(Dense(32, activation='relu', input_shape=(4,)))
-		self.model.add(Dense(3, activation='softmax'))
-		adam = Adam()
-		self.model.compile(loss='categorical_crossentropy',
-					optimizer=adam,
-					metrics=['accuracy'])
+    def define_model(self):
+        self.model.add(Dense(32, activation='relu', input_shape=(4,)))
+        self.model.add(Dense(3, activation='softmax'))
+        adam = Adam()
+        self.model.compile(loss='categorical_crossentropy',
+                           optimizer=adam,
+                           metrics=['accuracy'])
 
-		try:
-			self.model.load_weights('aq_model_weights.h5')
-		except IOError:
-			print('No file named aq_model_weights.h5')
+        try:
+            self.model.load_weights('aq_model_weights.h5')
+        except IOError:
+            print('No file named aq_model_weights.h5')
 
-	def train(self, y):
-		self.model.fit(self.x, y, epochs=1)
+    def train(self, y):
+        self.model.fit(self.x, y, epochs=1)
 
-	def predict(self):
-		return self.model.predict(self.x)
+    def predict(self):
+        return self.model.predict(self.x)
 
-	def grpc_stream(self):
-		channel = dial_grpc.dial()
-		stub = grpcapi_pb2_grpc.IoTStub(channel)
-		response = stub.GetNodeDataStream(grpcapi_pb2.GetNodeDataStreamInput())
+    def grpc_stream(self):
+        self.channel = dial_grpc.dial()
+        self.stub = grpcapi_pb2_grpc.IoTStub(self.channel)
+        self.response = self.stub.GetNodeDataStream(grpcapi_pb2.
+                                                    GetNodeDataStreamInput())
 
-		return response
+    def main(self):
+        t0 = time.time()
+        self.define_model()
+        self.read_node_ids()
+        self.grpc_stream()
 
-	def main(self):
-		t0 = time.time()
-		self.define_model()
-		self.read_node_ids()
-		response = self.grpc_stream()
+        for data in self.response:
+            print(data)
+            self.run(data)
+            if time.time() - t0 > 60:
+                print('Saving model...')
+                self.model.save_weights('aq_model_weights.h5')
+                t0 = time.time()
 
-		for data in response:
-			print(data)
-			self.run(data)
-			if time.time() - t0 > 60:
-				print('Saving model...')
-				self.model.save_weights('aq_model_weights.h5')
-				t0 = time.time()
-
-		channel.close()
+        self.channel.close()
--- test/test_alarm_level.py
+++ test/test_alarm_level.py
@@ -1,33 +1,35 @@
 import json
 import unittest
-import alarm_level.alarm_level
 from alarm_level.alarm_level import AlarmLevel
+
 
 class TestAlarmLevel(unittest.TestCase):
 
-	def test_no_asset(self):
-		self.test_1 = AlarmLevel('', 'test/test_spacing.csv')
-		self.test_2 = AlarmLevel('not present', 'test/test_spacing.csv')
-		with self.assertRaises(KeyError):
-			self.test_1.read_node_ids()
-		self.assertRaises(KeyError, self.test_2.read_node_ids)
+    def test_no_asset(self):
+        self.test_1 = AlarmLevel('', 'test/test_spacing.csv')
+        self.test_2 = AlarmLevel('not present', 'test/test_spacing.csv')
+        with self.assertRaises(KeyError):
+            self.test_1.read_node_ids()
+        self.assertRaises(KeyError, self.test_2.read_node_ids)
 
-	def test_csv_spacing(self):
-		self.test = AlarmLevel('test', 'test/test_spacing.csv')
-		self.test.read_node_ids()
-		self.assertDictEqual({'1': 'uuid_1', '2': 'uuid_2'}, self.test.node_ids)
+    def test_csv_spacing(self):
+        self.test = AlarmLevel('test', 'test/test_spacing.csv')
+        self.test.read_node_ids()
+        self.assertDictEqual({'1': 'uuid_1', '2': 'uuid_2'},
+                             self.test.node_ids)
 
-	def test_csv_columns(self):
-		self.test = AlarmLevel('test', 'test/test_columns.csv')
-		self.test.read_node_ids()
-		self.assertDictEqual({'2': 'uuid_2'}, self.test.node_ids)
+    def test_csv_columns(self):
+        self.test = AlarmLevel('test', 'test/test_columns.csv')
+        self.test.read_node_ids()
+        self.assertDictEqual({'2': 'uuid_2'}, self.test.node_ids)
 
-	def test_run(self):
-		with open('test/test_response.json') as f:
-			json_file = f.read()
+    def test_run(self):
+        with open('test/test_response.json') as f:
+            json_file = f.read()
 
-		data = DotDict(json.loads(json_file))
+        DotDict(json.loads(json_file))
+
 
 class DotDict(dict):
-	def __getattribute__(self, name):
-		return self[name]
+    def __getattribute__(self, name):
+        return self[name]
