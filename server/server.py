ff934a47c9925b65058fa869d42a3767c063a610
Victor Risne <risne@pm.me>
2018-07-31 14:15:32 +0200

    Add asset filtering from server side

--- server/server.py
+++ server/server.py
@@ -5,11 +5,24 @@ import json
 import grpcapi_pb2
 import grpcapi_pb2_grpc
 import dial_grpc
+import csv
+from collections import defaultdict
 
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 CORS(app)
+
+
+def read_ids(csv_file):
+    node_ids = defaultdict(dict)
+
+    with open(csv_file, 'r') as f:
+        next(f)
+        for row in csv.reader(f, skipinitialspace=True, delimiter=','):
+            node_ids[row[0]].update({row[1]: row[2]})
+
+    return node_ids
 
 
 def sse_pack(d):
@@ -20,27 +33,30 @@ def sse_pack(d):
     return buffer + '\n'
 
 
-@app.route('/grpc')
-def grpc_generator():
+@app.route('/grpc/<asset>')
+def grpc_generator(asset):
     channel = dial_grpc.dial()
     stub = grpcapi_pb2_grpc.IoTStub(channel)
     stream = stub.GetNodeDataStream(grpcapi_pb2.
                                     GetNodeDataStreamInput())
 
+    node_ids = list(read_ids('node_ids.csv')[asset].values())
+
     def event_stream(event_id, channel):
         for data in stream:
-            if data.node_data.data_point.coordinate.y == 0:
-                node_data = data.node_data.question_answers[0]
-            else:
-                node_data = data.node_data.data_point.coordinate.y
-            msg = ({'event': 'delta',
-                    'data': json.dumps({
-                        'node_id': data.node_id,
-                        'node_data': node_data
-                        }),
-                    'id': event_id})
-            yield sse_pack(msg)
-            event_id += 1
+            if any([data.node_id == node_id for node_id in node_ids]):
+                if data.node_data.data_point.coordinate.y == 0:
+                    node_data = data.node_data.question_answers[0]
+                else:
+                    node_data = data.node_data.data_point.coordinate.y
+                msg = ({'event': 'delta',
+                        'data': json.dumps({
+                            'node_id': data.node_id,
+                            'node_data': node_data
+                            }),
+                        'id': event_id})
+                yield sse_pack(msg)
+                event_id += 1
         channel.close()
 
     return Response(event_stream(0, channel), mimetype='text/event-stream')
