47fe685bb9b4e9f81cfa64f11837e73c940c3452
Victor Risne <risne@pm.me>
2018-08-02 10:34:25 +0200

    Change to .json config

--- node_ids.py
+++ node_ids.py
@@ -2,52 +2,42 @@ import dial_grpc
 import grpcapi_pb2
 import grpcapi_pb2_grpc
 import argparse
+import json
 
 
-SHORTHAND = {'temperature': 'temp',
-             'humidity': 'hum',
-             'pressure': 'pres',
-             'gas': 'gas',
-             'vote': 'vote',
-             'prediction': 'pred'}
-
-
-def generate_dict(asset='', parent_id=''):
+def generate_json(node_ids={}, fl='', asset='', parent_id=''):
     response = stub.GetChildNodes(grpcapi_pb2.PrimitiveString(value=parent_id))
+
     try:
         nodes = response.ListFields()[0][1]
         for node in nodes:
-            if node.type == 'asset':
-                asset = node.label.lower()
-                node_ids[asset] = {}
+            if node.type == 'functional_location':
+                fl = node.label.lower().replace(' ', '_')
+                node_ids[fl] = {}
+            elif node.type == 'asset':
+                asset = node.label.lower().replace(' ', '_')
+                node_ids[fl].update({asset: {}})
             else:
-                node_ids[asset].update({node.label.lower(): node.id})
-            generate_dict(asset, node.id)
+                ip = node.label.lower().replace(' ', '_')
+                node_ids[fl][asset].update({ip: node.id})
+            generate_json(node_ids, fl, asset, node.id)
 
+        return node_ids
     except:
         pass
 
-
-def generate_csv():
-    with open('node_ids.csv', 'w') as f:
-        f.write('Asset,Inspection Point,Node ID\n')
-        for asset in node_ids:
-            for inspection_point in node_ids[asset]:
-                ip = SHORTHAND[inspection_point]
-                f.write('{},{},{}\n'.format(asset,
-                                            ip,
-                                            node_ids[asset][inspection_point]))
-
-
-channel = dial_grpc.dial()
-stub = grpcapi_pb2_grpc.HierarchyStub(channel)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--id', help='node id from selected function location')
 args = parser.parse_args()
 
-node_ids = {}
-generate_dict(parent_id=args.id)
-generate_csv()
+channel = dial_grpc.dial()
+stub = grpcapi_pb2_grpc.HierarchyStub(channel)
+root_id = stub.GetParentNode(grpcapi_pb2.PrimitiveString(value=args.id)).id
+
+data = generate_json(parent_id=root_id)
+
+with open('config.json', 'w') as f:
+    json.dump(data, f, indent=2)
 
 channel.close()
