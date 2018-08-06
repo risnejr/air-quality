f0c4ffe3e8e91bc982c02d130a4e54e350e5ad5f
Victor Risne <risne@pm.me>
2018-08-06 10:27:31 +0200

    Update tests for aq_level

--- aq_level/test/test_aq_level.py
+++ aq_level/test/test_aq_level.py
@@ -5,34 +5,24 @@ from aq_level.aq_level import AQLevel
 
 class TestAQLevel(unittest.TestCase):
 
-    def test_no_csv(self):
-        self.assertRaises(FileNotFoundError, AQLevel,
-                          '', 'test/non_existing_csv.csv')
-        self.assertRaises(FileNotFoundError, AQLevel, '', '')
 
     def test_no_asset(self):
-        test_1 = AQLevel('', 'test/test_key.csv')
-        test_2 = AQLevel('not present', 'test/test_spacing.csv')
+        with open('../config.json', 'r') as f:
+            node_ids = json.load(f)
+        test_1 = AQLevel('install_team_room', '', node_ids)
+        test_2 = AQLevel('nope, not here', 'not present', node_ids)
         self.assertRaises(KeyError, test_1.asset_ids)
         self.assertRaises(KeyError, test_2.asset_ids)
 
-    def test_csv_spacing(self):
-        test = AQLevel('test', 'test/test_spacing.csv')
-        test.read_ids()
-        self.assertDictEqual({'test': {'1': 'uuid_1', '2': 'uuid_2'}},
-                             test.node_ids)
-
-    def test_csv_columns(self):
-        test = AQLevel('test', 'test/test_columns.csv')
-        test.read_ids()
-        self.assertDictEqual({'test': {'2': 'uuid_2'}}, test.node_ids)
 
     def test_run(self):
-        test = AQLevel('test', 'test/test_key.csv')
-        with open('test/test_response.json') as f:
-            json_file = f.read()
+        with open('test/test_config.json', 'r') as f:
+            node_ids = json.load(f)
 
-        data = DotDict(json.loads(json_file))
+        test = AQLevel('test', 'test', node_ids)
+        with open('test/test_response.json', 'r') as f:
+            data = DotDict(json.load(f))
+
         self.assertRaises(KeyError, test.run, data)
 
 
